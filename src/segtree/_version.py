55c5426ee83360068f63b8b5cf55c835da955c83
hereariim <herearii.metuarea@gmail.com>
2025-09-10 17:38:53 +0200

    git samhq removed

--- src/segtree/_version.py
+++ src/segtree/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.4.dev0+g4cab2c53f.d20250910'
-__version_tuple__ = version_tuple = (0, 0, 4, 'dev0', 'g4cab2c53f.d20250910')
+__version__ = version = '0.0.1'
+__version_tuple__ = version_tuple = (0, 0, 1)
 
-__commit_id__ = commit_id = 'g4cab2c53f'
+__commit_id__ = commit_id = None
